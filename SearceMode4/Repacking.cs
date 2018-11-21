9e67ee21d820dc952c885110beec857e01bebee4
Ревякин Андрей Владимирович <andrevyakin@hotmail.com>
2018-11-21 07:15:10 +0300

    Repacking

---
+++ SearceMode4/Repacking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Rar;
+
+namespace SearceMode4
+{
+    class Repacking
+    {
+        public static void RepackingRar(string path)
+        {
+            using (var archive = RarArchive.Open(path))
+            {
+                foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
+                {
+                   
+                }
+            }
+        }
+    }
+}
